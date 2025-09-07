4d53ea17363bdd3c5d24a130ac387599ada84064
Alex Carran <146670072+code-by-alex@users.noreply.github.com>
2025-09-07 19:15:17 -0400

    Update ProjectHeader.tsx

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -27,7 +27,7 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
       />
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <Header
-          name="Product Design Development"
+          name="FW Legal Recruiting Development Team"
           buttonComponent={
             <button
               className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
